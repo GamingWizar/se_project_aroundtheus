d144d26c8ebbf78068e4f5adb321f6053d1f7026
Carver Hannasch <awizard1011@gmail.com>
2024-02-07 16:57:42 -0500

    add modal functionality and cards generated through javascript

--- scripts/index.js
+++ scripts/index.js
@@ -25,14 +25,50 @@ let initialCards = [
   }),
 ];
 
+function getCardElement(data) {
+  let cardTemplate = document.querySelector("#cardTemplate").content;
+  let cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  let cardName = data.name;
+  let cardImage = data.link;
+  cardElement.querySelector(".card__text").textContent = cardName;
+  cardElement.querySelector(".card__image").src = cardImage;
+  cardElement.querySelector(".card__image").alt = cardName;
+  return cardElement;
+}
+
+let cardList = document.querySelector(".cards");
+
+for (let i = 0; i < initialCards.length; i++) {
+  cardList.append(getCardElement(initialCards[i]));
+}
+
 let modal = document.querySelector(".modal");
-document
+let modalName = modal.querySelector(".modal__input_name");
+let modalDescription = modal.querySelector(".modal__input_description");
+let profile = document.querySelector(".profile");
+let profileName = profile.querySelector(".profile__name");
+let profileDecsription = profile.querySelector(".profile__description");
+profile
   .querySelector(".profile__edit")
   .addEventListener("click", function (event) {
     modal.classList.add("modal_opened");
+    modalName.value = profileName.textContent;
+    modalDescription.value = profileDecsription.textContent;
   });
 
 let modalCloseButton = modal.querySelector(".modal__close");
 modalCloseButton.addEventListener("click", function (event) {
   modal.classList.remove("modal_opened");
 });
+
+let profileForm = modal.querySelector(".modal__form");
+
+function handleProfileFormSubmit(event) {
+  event.preventDefault();
+  console.log("Form Submitting!");
+  profileName.textContent = modalName.value;
+  profileDecsription.textContent = modalDescription.value;
+  modal.classList.remove("modal_opened");
+}
+
+profileForm.addEventListener("submit", handleProfileFormSubmit);
